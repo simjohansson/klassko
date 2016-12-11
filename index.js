a0f3b59b9aa97584e18efcbd8ce9eda79048d2fa
simjohansson <simjohansson@gmail.com>
2016-12-11 20:26:00 +0100

    small port change.

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + 'public/index.html');
 });
+
+http.listen(PORT, function () {
+   console.log(`Listening on ${PORT}`); 
+  });
 
 var roomModule = require('./roomModule');
 
@@ -45,8 +50,4 @@ io.on('connection', function (socket) {
     }
   });
 
-});
-
-http.listen(3000, function () {
-  console.log('listening on *:3000');
 });
